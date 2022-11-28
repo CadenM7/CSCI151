044eb83dd56bcb6cf76d95bdb1153daf03f2d125
Caden McCarty <mccartycl@hendrix.edu>
2022-11-28 10:57:42 -0600

    Added Class and changed the Blackjack game

--- Blackjack.java
+++ Blackjack.java
@@ -1,26 +1,49 @@
 package Game;
 
-import java.util.Random;
-
 public class Blackjack {
 
     private int player = 0;
 
     private int dealer = 0;
 
-    private BlackjackStack deck;
+    private boolean draw;
+
+    private boolean lose;
+
+    private boolean won;
+
+    private CardValues values;
+
+    private CardRandomizer shuffle;
 
     private int size;
 
 
-    public Blackjack() {
-        
+    public Blackjack(int size) {
+        this.size = size;
+
+        if (player == 21) {
+            won = true;
+        }
+        if (dealer == 21) {
+            lose = true;
+        } else if (player > 21 || dealer > 21) {
+            draw = true;
+        } else if (player > 21 && dealer < 21) {
+            lose = true;
+        } else if (player < 21 && dealer > 21) {
+            won = true;
+        }
     }
 
     public String toString() {
 
+        return null;
     }
 
-    public void randomize()
+    public void dealer() {
 
+    }
 }
+
+
---
+++ CardRandomizer.java
@@ -0,0 +1,13 @@
+package Game;
+
+import java.util.Random;
+
+public class CardRandomizer {
+
+    private BlackjackStack deck;
+
+
+    public CardRandomizer() {
+
+    }
+}
